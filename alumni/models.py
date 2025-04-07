42926074930b2a79bbe7ad3ff01a18933d8ebabd
Devendra <devendra12052005@gmail.com>
2025-04-07 12:13:45 +0530

    Added Database to store and check for duplicate emails during account creation

--- alumni/models.py
+++ alumni/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    role=models.CharField(max_length=10)
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=50)
+    email=models.EmailField()
+    password=models.CharField(max_length=50)
+    graduation_year=models.IntegerField(null=True,blank=True)
+    degree=models.CharField(max_length=50,null=True,blank=True)
+
+    def __str__(self):
+        return self.first_name
--- alumni/views.py
+++ alumni/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import student
 
 # Create your views here.
 # Displaying Homepage
@@ -18,10 +19,21 @@ def create_account(request):
     lastname=request.POST.get('last_name')
     email=request.POST.get('email')
     password=request.POST.get("password")
-    gradyear=request.POST.get("grad_year",'default')
-    degree=request.POST.get('degree','default')
-    params={'role':role,'firstname':firstname,'lastname':lastname,'email':email,'password':password,'gradyear':gradyear,'degree':degree}
-    return render(request,'alumni/data.html',params)
+    gradyear=request.POST.get("grad_year",'')
+    degree=request.POST.get('degree','')
+    data=student.objects.all()
+    if gradyear.isdigit():
+        gradyear=int(gradyear)
+    else:
+        gradyear=None
+    if degree=='':
+        degree=None
+    for saved_email in data:
+        if(email==saved_email.email):
+            return HttpResponse("Email Id already exists")
+    student.objects.create(role=role,first_name=firstname,last_name=lastname,email=email,password=password,graduation_year=gradyear,degree=degree)
+    return HttpResponse("Account Created Succesfully")
+
 # Module for Signin Data
 def signindata(request):
     email=request.POST.get('email')
