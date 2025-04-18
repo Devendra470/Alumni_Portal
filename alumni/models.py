3f5a8be2dfda65d46a576c8f67800301e8694c0b
Devendra470 <devendra12052005@gmail.com>
2025-04-18 17:21:53 +0530

    Changed Databsae

--- alumni/models.py
+++ alumni/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class user(models.Model):
+class User(models.Model):
     role=models.CharField(max_length=10)
     first_name=models.CharField(max_length=100)
     last_name=models.CharField(max_length=50)
